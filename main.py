57d82177e6b1242c4521a8818b49de581ab6a599
Florian Briand <florian.briand@bepatient.com>
2015-05-23 18:57:48 +0200

    Implement base templates

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
+from hls_server.server import HlsServerView
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Hello World!"
+app.add_url_rule('/',
+	view_func=HlsServerView.as_view('hls_server_view'),
+    methods=['GET'])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
